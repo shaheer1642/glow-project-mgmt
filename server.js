1a776587419d612cd36954fad6a2b1c114ef6ee6
shaheer1642 <shaheer1642@gmail.com>
2023-10-23 14:26:16 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
 
 app.use('/api', require('./routes/reliefweb'))
 
+
+app.use(express.static(path.join(__dirname, 'front_end', 'build')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+});
+
 const port = process.env.port || 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
