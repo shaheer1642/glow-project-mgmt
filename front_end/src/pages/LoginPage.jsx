429336824776fc78e1e2e3f5b0215f02ef70c002
Adila-Jaffar <148770541+Adila-Jaffar@users.noreply.github.com>
2023-10-25 11:54:58 +0500

    login_page_changes

--- front_end/src/pages/LoginPage.jsx
+++ front_end/src/pages/LoginPage.jsx
@@ -6,23 +6,24 @@ function LoginPage() {
     const [count, setCount] = useState(0)
     const navigate = useNavigate()
     return (
-        <Grid container minHeight={'100%'} display='flex' alignItems={'center'} justifyContent={'center'}>
-            <Grid container item xs={'auto'} gap={3} padding={3} flexDirection={'column'} display='flex' alignItems={'center'} justifyContent={'center'} border='3px solid skyblue'>
-                <Grid item xs={12}>
-                    <Typography>Please Login to Access the Dashboard</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField variant='outlined' label='Username' />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField variant='outlined' label='Password' />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant='contained' onClick={() => navigate('/dashboard')}>Login</Button>
-                </Grid>
+        <Grid container  padding={3}> 
+        <Grid container item xs={6} justifyContent={"right"}>
+            <img src="/images/login_bg.JPG" width={300}></img>        
+            </Grid>
+        <Grid container item xs={3} bgcolor={"grey"} alignItems={'center'} padding={3}>
+            <Grid item xs={12}>
+                <TextField variant='outlined' label="Username"/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField variant='outlined' label="Password"/>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="contained" onClick={() => navigate('/dashboard')}>Login</Button>
             </Grid>
         </Grid>
+        </Grid> 
     )
+
 }
 
 export default LoginPage
