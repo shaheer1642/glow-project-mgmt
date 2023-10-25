5ba87c3c0d227f8e8ddb8d36a270611f8e00855e
Adila-Jaffar <148770541+Adila-Jaffar@users.noreply.github.com>
2023-10-25 12:44:04 +0500

    new

--- front_end/src/layouts/HomeLayout.jsx
+++ front_end/src/layouts/HomeLayout.jsx
@@ -1,29 +1,125 @@
-import { Button, Typography, Grid, AppBar } from '@mui/material'
-import { useState } from 'react'
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Outlet } from 'react-router-dom';
 
-function HomeLayout() {
-    const [count, setCount] = useState(0)
-    const navigate = useNavigate()
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact','FAQS','Login'];
 
-    return (
-        <Grid container minWidth={'100vw'} minHeight={'100vh'}>
-            <Grid container item xs={12} marginBottom={'auto'} bgcolor='skyblue' borderBottom={'2px blue solid'}>
-                <Grid item xs={'auto'} marginLeft={'auto'}>
-                    <Link to='/'>Project Management System</Link>
-                </Grid>
-                <Grid item xs={'auto'} marginLeft={'auto'}>
-                    <Button variant='outlined' onClick={() => navigate('login')}>Login</Button>
-                </Grid>
-            </Grid>
-            <Grid item xs={12}>
-                <Outlet />
-            </Grid>
-            <Grid item xs={12} marginTop={'auto'} bgcolor='skyblue' borderTop={'2px blue solid'}>
-                <Typography>This is the footer</Typography>
-            </Grid>
-        </Grid>
-    )
+function HomeLayout(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        GLOW Consultants
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img
+            src='/images/logo_dark.png'
+            height={50}
+            width={100}
+            component="div"
+                      />
+          <Box marginLeft={"auto"} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Outlet />
+      </Box>
+      <Grid container>
+        <Grid item xs={12} bgcolor='darkblue'>
+          <img src='/images/logo_dark.png' />
+          </Grid>
+        <Grid item xs={12}>
+          </Grid>
+        <Grid item xs={12}>
+          </Grid>
+        <Grid item xs={12}>
+          </Grid>
+      </Grid>
+        
+      </Box>
+  );
+  
 }
 
-export default HomeLayout
+HomeLayout.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default HomeLayout;
